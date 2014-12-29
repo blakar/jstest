722b955c6c0e94663585bf5bc113c0dcd4ef638c
blakar <blakar@post.cz>
2014-12-29 13:08:11 -0800

    Add few more basic tests

--- basic/tests.js
+++ basic/tests.js
@@ -1,3 +1,20 @@
 QUnit.test( "hello test", function( assert ) {  
 	assert.ok( 1 == "1", "Passed!" );
 });
+
+
+QUnit.test("Addition works", function(assert){
+	assert.strictEqual(1+1, 2, "1+1 === 2");
+});
+
+QUnit.test("Addition works", function(assert){
+	assert.equal(10+10, 20, "10+10 == 20");
+});
+
+QUnit.test("Zero is false (non-strict)", function(assert){
+	assert.equal(0, false, "0 == false");
+});
+
+QUnit.test("Zero is strictly not false", function(assert){
+	assert.notStrictEqual(0, false, "0 !=== false");
+});
