ebeaf87b1d66d71abdc2300c4bb4cb002c88339b
blakar <blakar@post.cz>
2015-01-18 22:01:25 -0800

    Add test for _.pluck

--- underscoretests/tests.js
+++ underscoretests/tests.js
@@ -25,3 +25,25 @@ QUnit.test("select test 2", function(assert){
 	assert.strictEqual(evenScores[0], 8, "First item is correct");
 	assert.strictEqual(evenScores[1], 8, "Second item is correct");
 });
+
+// pluck - extracting specific property from each object
+QUnit.test("select test 2", function(assert){
+	var locations =
+	[
+		{city: 'Seattle', state: 'Washington'},
+		{city: 'New York City', state: 'New York'},
+		{city: 'Los Angeles', state: 'California'},
+		{city: 'Anchorage', state: 'Alaska'},
+		{city: 'Austin', state: 'Texas'},
+	];
+	
+	var cities = _.pluck(locations, 'city');
+	var states = _.pluck(locations, 'state');
+	
+	assert.strictEqual(cities.length, 5, "Correct number of cities");
+	assert.strictEqual(cities[0], 'Seattle', 'First city is correct');
+	assert.strictEqual(cities[2], 'Los Angeles', 'Third city is correct');
+	assert.strictEqual(states.length, 5, 'Correct number of states');
+	assert.strictEqual(states[0], 'Washington', 'First state is correct');
+	assert.strictEqual(states[4], 'Texas', 'Fifth state is correct');
+});
